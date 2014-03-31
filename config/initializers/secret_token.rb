38359caac0cea75bd44e1314757ae484a384b3ef
Agolveo <agolveo@gmail.com>
2014-03-31 13:41:20 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Badger::Application.config.secret_key_base = '15c9c9387fe4fea6934d299fbc3e76092131fd34984f1834453b7e1fa26c5736e539f0aae19ccdf32a633659db8c367c9268eef4db352d2756e6f1d6904aadb8'
