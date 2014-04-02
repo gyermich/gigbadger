6ae854e86ca1c2bd70237da52a8594b1f4916b08
Galina <yermichevagalina@gmail.com>
2014-04-02 09:24:59 -0700

    added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Badger::Application.routes.draw do
+
+  root "tasks/index"
+
+  get "session/create"
+  get "session/destroy"
+
   resources :categories
-
   resources :tasks
-
   resources :users
 
-  
+
 end
