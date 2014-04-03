4b2b90f69b7f801fd3f553869552e76668eceb86
Galina <yermichevagalina@gmail.com>
2014-04-03 14:39:07 -0700

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,15 @@ Badger::Application.routes.draw do
   get "session/create"
   get "session/destroy"
   get "session/destroy"
-  # get "/login", to: "users#new"
-  # post "in", to: "session#create",     as: "log_in"
-  # delete "logout", to: "session#destroy", as: "logout"
 
-  post "login"  => "session#create",     :as => "login"
-  get "logout" => "session#destroy", :as => "logout"
-  get "signup" => "users#new",       :as => "signup"
+  post "login", "session#create",  as: "login"
+  get "logout", "session#destroy", as: "logout"
+  get "signup", "users#new",       as: "signup"
 
   resources :categories
   resources :tasks
   resources :users
-  # resources :session
+
 
 
 end
