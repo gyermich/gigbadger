a116a2b7dd03d0f7f412899dbfc701c0d0817fdc
Agolveo <agolveo@gmail.com>
2014-04-07 15:09:58 -0700

    creating task with categories works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,15 +8,12 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
-    @categories = Category.all
   end
 
   def create
-    # @category = Category.find(params[:category])
     @task = Task.create(task_params)
     if @task.save
-      @task.categories = params[:category_id].present? ? Category.find(params[:category]) : [ ] 
-      # TaskCategory.create_record(@task, @category)
+      @task.categories = params[:task][:categories].present? ? Category.find_all_by_id(params[:task][:categories]) : [ ] 
       redirect_to task_path(@task), notice: "Yay! Your gig is posted!"
     else
       render :new
