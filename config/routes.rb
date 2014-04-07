541bb5c642be66689921c3e123f4547de7206352
Galina <yermichevagalina@gmail.com>
2014-04-06 19:24:23 -0700

    changes to sign-up form

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,7 @@ Badger::Application.routes.draw do
   root "welcome#index"
 
   get "welcome/index"
-  # get "session/create"
-  # get "session/destroy"
-  # get "session/destroy"
 
-  # post "login", to: "session#create",  as: "login"
-  # get "logout", to: "session#destroy", as: "logout"
-  # get "signup", to: "users#new",       as: "signup"
 
   resources :categories
   resources :tasks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406231058) do
+ActiveRecord::Schema.define(version: 20140407002636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 20140406231058) do
     t.text     "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
