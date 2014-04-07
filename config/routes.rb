b61f3776a309b0b478687976e06bb19086145ac9
Galina <yermichevagalina@gmail.com>
2014-04-06 20:15:07 -0700

    edited devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Badger::Application.routes.draw do
 
   devise_for :users
-  # resources :users, only: [:index]
+
   root "welcome#index"
-
   get "welcome/index"
-
 
   resources :categories
   resources :tasks
