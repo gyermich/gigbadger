e41eebb8781c06f76910c957c82537bc2a031350
Galina <yermichevagalina@gmail.com>
2014-04-09 15:22:32 -0700

    styled filter for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,16 +17,10 @@ class Task < ActiveRecord::Base
   )
     scope :search_query, lambda { |query|
       return nil  if query.blank?
-         # condition query, parse into individual keywords
          terms = query.downcase.split(/\s+/)
-         # replace "*" with "%" for wildcard searches,
-         # append '%', remove duplicate '%'s
          terms = terms.map { |e|
            (e.gsub('*', '%') + '%').gsub(/%+/, '%')
          }
-         # configure number of OR conditions for provision
-         # of interpolation arguments. Adjust this if you
-         # change the number of OR conditions.
          num_or_conditions = 2
          Task.where(
            terms.map {
