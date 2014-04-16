f6087bf57d5f4092347518c960eb6110719e967c
Galina <yermichevagalina@gmail.com>
2014-04-16 14:50:37 -0700

    styled edit profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :bio, :city, :zipcode, :image) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name) }
     end
 end
