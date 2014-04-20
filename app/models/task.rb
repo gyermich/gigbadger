d5c07f5d2d89ba60eb0938e453b5a3428a665f9b
Galina <yermichevagalina@gmail.com>
2014-04-19 19:07:47 -0700

    fixed task query bug

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,11 +12,11 @@ class Task < ActiveRecord::Base
   end
 
   def self.posted_tasks(user)
-    Task.where("owner_id = ? AND status = ? OR status = ?", user.id, "available", "in progress")
+    Task.where("owner_id = ? AND status = ? OR owner_id = ? AND status = ?", user.id, "available", user.id, "in progress")
   end
 
   def self.past_posted_tasks(user)
-    Task.where("owner_id = ? AND status = ? OR status = ?", user.id, "completed", "expired")
+    Task.where("owner_id = ? AND status = ? OR owner_id = ? AND status = ?", user.id, "completed", user.id, "expired")
   end
 
   def in_progress
