6f2ef36425cacad21a1701f258e9e16a5cb536d0
Galina <yermichevagalina@gmail.com>
2014-04-21 16:37:31 -0700

    fixed email job bug

--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -2,8 +2,8 @@ class EmailJob
 
   @queue = :email
   def self.perform(user_id, task_id)
-    user = User.find(user_id)
-    task = Task.find(task_id)
-    UserMailer.offer_accepted(user, task).deliver
+    @user = User.find(user_id)
+    @task = Task.find(task_id)
+    UserMailer.offer_accepted(@user, @task).deliver
   end
 end
