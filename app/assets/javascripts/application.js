b7c1796c67207021ecc8d0b0b5d285c6be441917
Galina <yermichevagalina@gmail.com>
2014-04-21 11:25:56 -0700

    fixed js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,31 +15,4 @@
 //= require bootstrap-datepicker
 //= require filterrific-jquery
 //= require_tree .
-$(document).ready(function() {
 
-  var checkbox = document.getElementById('paid');
-  var delivery_div = document.getElementById('pay');
-  var showHiddenDiv = function(){
-     if(checkbox.checked) {
-       delivery_div.style['display'] = 'block';
-     } else {
-       delivery_div.style['display'] = 'none';
-     }
-  }
-  checkbox.onclick = showHiddenDiv;
-  showHiddenDiv();
-
-
-  $('.js-datepicker').datepicker();
-   $('#overlayTrigger').click(function(event) {
-     event.preventDefault();
-     $('body').chardinJs('start');
-   });
-
-   close = document.getElementById("close");
-   close.addEventListener('click', function() {
-     note = document.getElementById("note");
-     note.style.display = 'none';
-   }, false);
-
-});
